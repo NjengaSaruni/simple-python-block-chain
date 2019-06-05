a8b71d7d70862b60443f0070d06578e2bce7a43f
Peter Njenga <peternjengask@gmail.com>
2019-06-05 14:15:58 +0300

    Add tests for valid blockchain validation

---
+++ README.md
@@ -0,0 +1,4 @@
+# A simple python blockchain
+
+This is a implementation of a blockchain using SHA256 hashing in Python.
+
--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+import copy
 from datetime import datetime
 import hashlib as hasher
 
@@ -41,10 +42,20 @@ def next_block(last_block, data=''):
                   previous_hash=last_block.hash)
     return block
 
+def validate_block_chain(blockchain):
+    index = 1
+
+    while index < len(blockchain):
+        if blockchain[index].previous_hash == blockchain[index - 1].hash:
+            index += 1
+        else:
+            break
+
+    return blockchain[0:index]
 
 class TestBlockChain(unittest.TestCase):
 
-    def test_chain(self):
+    def setUp(self) -> None:
         """Test creating chain of 20 blocks."""
         blockchain = [make_genesis_block()]
         prev_block = blockchain[0]
@@ -55,9 +66,24 @@ class TestBlockChain(unittest.TestCase):
             print('{} added to blockchain'.format(block))
             print('Hash: {}\n'.format(block.hash))
 
-        # Assert that a blockchain of length 20 was created
-        self.assertTrue(len(blockchain), 20)
+        self.blockchain = blockchain
 
+    def test_chain_length(self):
+        # Assert that a blockchain of length 20 was created
+        self.assertTrue(len(self.blockchain), 20)
+
+    def test_valid_chain(self):
+        self.assertEqual(self.blockchain, validate_block_chain(self.blockchain))
+
+
+    def test_invalid_chain(self):
+        blockchain_to_mutate = copy.deepcopy(self.blockchain)
+        block_to_mutate = blockchain_to_mutate[9]
+
+        block_to_mutate.data = 'Edited data'
+        block_to_mutate.hash = block_to_mutate.hash_block()
+
+        self.assertEqual(blockchain_to_mutate[0:10], validate_block_chain(blockchain_to_mutate))
 
 if __name__ == '__main__':
     unittest.main()
